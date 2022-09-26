76adc58d2abf5c7ce10dd295580efb493a720623
ganachecell <bluegag@gmail.com>
2022-09-26 23:21:50 +0900

    index2.html 추가, 시뮬레이팅 email,카카오톡 모달 추가

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+//------------------ Modal --------------------------
+$(".modal-open").on('click',function(){
+  $("#modal-background").fadeIn(300);
+  $(".modal-con").css("display", "flex").hide().fadeIn();
+  $("body").css("overflow", "hidden");
+});
+
+$("#modal-background, .close").on('click',function(){  
+  if ($(this).hasClass("close") || !$(this).next().hasClass('modal-progress'))
+  {
+    $("#modal-background").fadeOut(300);
+    $(".modal-con").fadeOut(300);  
+    $('body').css('overflow', 'overlay');
+  }
+});
